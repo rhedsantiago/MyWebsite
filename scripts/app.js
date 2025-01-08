f560f85c5b43589b4194bb4cae212d24127f8079
Rhed Santiago <rhedsantiago3@gmail.com>
2025-01-07 23:04:36 -0600

    Initialized app.js and added code to index.html and main.css

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function()){
+    const navLinks = document.querySelectorAll("nav ul li a");
+    navLinks.forEach(link =>
+        link.addEventL
+    )
+
+}
