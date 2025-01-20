6e04ad8e7eba332ec34e955d4344295dc084392f
Rhed Santiago <rhedsantiago3@gmail.com>
2025-01-20 02:11:07 -0600

    modified app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
-document.addEventListener("DOMContentLoaded", function()){
+document.addEventListener("DOMContentLoaded", function() {
     const navLinks = document.querySelectorAll("nav ul li a");
-    navLinks.forEach(link =>
-        link.addEventL
-    )
-
-}
+    navLinks.forEach(link => {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+            alert("You clicked a link!");
+        });
+    });
+});
