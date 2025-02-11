5ea7f794170d505eccd29abcc4af585dfd0e0804
Rhed Santiago <rhedsantiago3@gmail.com>
2025-02-10 22:49:48 -0600

    fixed some design issues, added a project section, and added the preliminaries of a carousel.js file

---
+++ scripts/carousel.js
@@ -0,0 +1,6 @@
+const prevButton = document.querySelector(".prev");
+const nextButton = document.querySelector(".next");
+
+const projectWrapper = document.querySelector(".projects-wrapper");
+
+let index = 0;
